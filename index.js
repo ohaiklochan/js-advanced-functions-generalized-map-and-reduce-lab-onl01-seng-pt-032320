96cef59b20bf027ae2cb9cfdd8110e9130865de3
Chloe Steffens <caforonda103@yahoo.com>
2020-10-25 22:08:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function reduce(array, callbackFunction, startingPoint) {
   let startAmount = (!!startingPoint) ? startingPoint : array[0]
   let i = (!!startingPoint) ? 0 : 1
   for (; i < array.length; i++) {
-    startingAmount = callbackFunction(array[i]. starAmount)
+    startAmount = callbackFunction(array[i]. startAmount)
   }
   return startAmount
 }
